9fc19380222aa5742a533c1fab3f0e5cf0dea3bc
Richard Walter <richardwalter515@gmail.com>
2020-11-09 14:01:36 -0500

    updated inquirer questions

--- Develop/app.js
+++ Develop/app.js
@@ -1,27 +1,111 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+// const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
 
 const employees = [];
 
 
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-inquirer
+inquirer.prompt([
+  {
+    type: "list",
+    message: "What kind of employee would you like to add?",
+    name: "employeeType",
+    choices: [
+      "Manager",
+      "Engineer",
+      "Intern"
+    ],
+  }]).then(function(employee){
+    if (employee.employeeType === "Manager") {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your name?"
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your employee ID?"
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email?"
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is your office number?"
+        },
+      ]).then(employees.push(employee));
+    } else if (employee.employeeType === "Engineer") {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your name?"
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your employee ID?"
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email?"
+        },
+        {
+          type: "input",
+          name: "gitHub",
+          message: "What is your GitHub username?"
+        },
+      ]).then(employees.push(employee));
+    } else {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your name?"
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your employee ID?"
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email?"
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "Where are you currently enrolled?"
+        },
+      ]).then(employees.push(employee));
+    }
+
+  });
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-render(employees);
+// render(employees);
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -32,7 +116,7 @@ render(employees);
 
   
   // Create manager, engineer, and interns
-  // const manager = new Employee("Richie", Engineer);
+  // const richie = new Engineer("richie", 1, "richie@email.com", "richiegithub");
   // const engineer = new Employee("Tim", Intern);
   // const intern = new Employee("Rachael", Manager);
 
