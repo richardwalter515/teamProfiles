e846f8a3a3050668a315500b41005fd08c552eef
Richard Walter <richardwalter515@gmail.com>
2020-11-10 22:15:17 -0500

    updated functions and layout

--- Develop/app.js
+++ Develop/app.js
@@ -12,123 +12,222 @@ const render = require("./lib/htmlRenderer");
 
 let employees = [];
 
-
+askQuestions();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-inquirer.prompt([
-  {
-    type: "list",
-    message: "What kind of employee would you like to add?",
-    name: "employeeType",
-    choices: [
-      "Manager",
-      "Engineer",
-      "Intern"
-    ],
-  }]).then(function(employee){
-    if (employee.employeeType === "Manager") {
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is your name?"
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "What is your employee ID?"
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is your email?"
-        },
-        {
-          type: "input",
-          name: "officeNumber",
-          message: "What is your office number?"
-        },
-      ]).then(function(employee) {
-        var manager = new Manager(employee.name, employee.id, employee.email, employee.officeNumber);
-        employees.push(manager);
-        var htmlText = render(employees);
+function askQuestions() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What kind of employee would you like to add?",
+      name: "employeeType",
+      choices: [
+        "Manager",
+        "Engineer",
+        "Intern"
+      ],
+    }]).then(function(employee){
+      employDetails(employee.employeeType) 
+    });
+  };
 
-        var filename = employee.name.toLowerCase().split(' ').join('') + ".json";
-        fs.writeFile(filename, htmlText, function(err) {
-          render(employees);
-          if (err) {
-            return console.log(err);
-          }
-          console.log("Created Employee Object!");
-        });
-      });
-    } else if (employee.employeeType === "Engineer") {
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is your name?"
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "What is your employee ID?"
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is your email?"
-        },
-        {
-          type: "input",
-          name: "gitHub",
-          message: "What is your GitHub username?"
-        },
-      ]).then(function(data) {
-        var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-          if (err) {
-            return console.log(err);
-          }
-          console.log("Created Employee Object!");
-        });
-      });
-    } else {
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is your name?"
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "What is your employee ID?"
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is your email?"
-        },
-        {
-          type: "input",
-          name: "school",
-          message: "Where are you currently enrolled?"
-        },
-      ]).then(function(data) {
-        var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-          if (err) {
-            return console.log(err);
-          }
-          console.log("Created Employee Object!");
-        });
-      });
+    function employDetails(role) {
+      if (role === "Manager") {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "What is your employee ID?"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+          },
+          {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number?"
+          },
+        ]).then(function(employee) {
+          var manager = new Manager(employee.name, employee.id, employee.email, employee.officeNumber);
+          employees.push(manager);
+        })
+      } else if (role === "Engineer") {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "What is your employee ID?"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+          },
+          {
+            type: "input",
+            name: "gitHub",
+            message: "What is your GitHub username?"
+          },
+        ]).then(function(employee) {
+          var engineer = new Engineer(employee.name, employee.id, employee.email, employee.gitHub);
+          employees.push(engineer);
+          console.log('employees:', employees)
+        })
+      } else {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "What is your employee ID?"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+          },
+          {
+            type: "input",
+            name: "school",
+            message: "Where are you currently enrolled?"
+          },
+        ]).then(function(employee) {
+          var intern = new Intern(employee.name, employee.id, employee.email, employee.school);
+          employees.push(intern);
+          var htmlText = render(employees);
+          var filename = "team.html";
+          fs.writeFile(filename, htmlText, function(err) {
+            if (err) {
+              return console.log(err);
+            }
+            console.log("Created Employee HTML!");
+          });
+        })
+      }
     }
-
-  });
-
   
+    //   if (employee.employeeType === "Manager") {
+        
+  
+    //     })
+    //     inquirer.prompt([
+    //       {
+    //         type: "list",
+    //         message: "Would you like to add another employee?",
+    //         name: "nextEmp",
+    //         choices: [
+    //           "Yes",
+    //           "No",
+    //         ],
+    //       }
+    //     ]).then(function(want) {
+    //       console.log('want:', want)
+    //       if (want.nextEmp === "Yes") {
+    //         askQuestions();
+    //       } else {
+            // var htmlText = render(employees);
+            // var filename = "team.html";
+            // fs.writeFile(filename, htmlText, function(err) {
+            //   if (err) {
+            //     return console.log(err);
+            //   }
+            //   console.log("Created Employee HTML!");
+            // });
+          // }
+    //     }
+    //     )
+    //   } else if (employee.employeeType === "Engineer") {
+    //     inquirer.prompt([
+          
+    //     ]).then(function(employee) {
+    //       var manager = new Manager(employee.name, employee.id, employee.email, employee.officeNumber);
+    //       employees.push(manager);
+  
+    //     }).then(inquirer.prompt([
+    //       {
+    //         type: "list",
+    //         message: "Would you like to add another employee?",
+    //         name: "nextEmp",
+    //         choices: [
+    //           "Yes",
+    //           "No",
+    //         ],
+    //       }
+    //     ])).then(function(want) {
+    //       if (want.nextEmp === "Yes") {
+    //         askQuestions();
+    //       } else {
+    //         var htmlText = render(employees);
+    //         var filename = "team.html";
+    //         fs.writeFile(filename, htmlText, function(err) {
+    //           if (err) {
+    //             return console.log(err);
+    //           }
+    //           console.log("Created Employee HTML!");
+    //         });
+    //       }
+    //     }
+    //     )
+    //   } else {
+    //     inquirer.prompt([
+          
+    //     ]).then(function(employee) {
+    //       var manager = new Manager(employee.name, employee.id, employee.email, employee.officeNumber);
+    //       employees.push(manager);
+  
+    //     }).then(inquirer.prompt([
+    //       {
+    //         type: "list",
+    //         message: "Would you like to add another employee?",
+    //         name: "nextEmp",
+    //         choices: [
+    //           "Yes",
+    //           "No",
+    //         ],
+    //       }
+    //     ])).then(function(want) {
+    //       if (want.nextEmp === "Yes") {
+    //         askQuestions();
+    //       } else {
+    //         var htmlText = render(employees);
+    //         var filename = "team.html";
+    //         fs.writeFile(filename, htmlText, function(err) {
+    //           if (err) {
+    //             return console.log(err);
+    //           }
+    //           console.log("Created Employee HTML!");
+    //         });
+    //       }
+    //     }
+    //     )
+    //   }
+    // }
+
+
+  // var htmlText = render(employees);
+  // var filename = "team.html";
+  // fs.writeFile(filename, htmlText, function(err) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  //   console.log("Created Employee Object!");
+  // });
   
   // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
